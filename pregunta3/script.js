3baad9a053db0ea806834d686e95ffdcf0a10f22
Rodrigo <rodrigosolorzano4@hotmail.com>
2021-02-23 21:01:51 -0600

    examen parcial

--- pregunta3/script.js
+++ pregunta3/script.js
@@ -1,21 +1,23 @@
 function sumarNumeros() {
-    var N1, N2, suma, text;
-
-    Numero1 = document.getElementById("Num1").value;
-    Numero2 = document.getElementById("Num2").value;
+    var Numero1 = document.getElementById("Num1").value;
+    var Numero2 = document.getElementById("Num2").value;
 
     if (isNaN(Numero1) || isNaN(Numero2)) {
 
     } else {
         //si no ponemos parseFloat concatenaría x con y  
-        suma = parseFloat(Numero1) + parseFloat(Numero2);
-        text = suma;
+        var suma = parseFloat(Numero1) + parseFloat(Numero2);
     }
 
-    var fila = "<tr><td>" + numero1 + "</td><td>" + numero2 + "</td><td>" + text + "</td></tr>" + "</td><td>" + 1 + "</td></tr>";
+    if (suma < 10) {
+        var status = "10-"
+    } else if (suma = 10) {
+        var status = "10="
+    } else if (suma > 10) {
+        var status = "10+"
+    }
 
-    var btn = document.createElement("TR");
-    btn.innerHTML = fila;
-    return document.getElementById("tablita").appendChild(btn);
+    return document.getElementById("tabla").insertRow(-1) = "<tr><td>" + numero1 + "</td><td>" + numero2 + "</td><td>" + suma + "</td></tr>" + "</td><td>" + status + "</td></tr>";;
+
 
 }
